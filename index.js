17b473ad85e42911bd03aae23304373cae366171
codescientists <piyushpp2412@gmail.com>
2024-06-10 12:44:36 +0530

    Added routes

--- index.js
+++ index.js
@@ -8,18 +8,19 @@
 
 // app.use(express.json());
 
-// const orderRoutes = require('./routes/orderRoutes');
-
-// app.use('/api/orders', orderRoutes);
 
 // // Start the server
 // app.listen(PORT, () => {
-//   console.log(`Server is running on ${process.env.BASE_URL}:${PORT}`);
-// });
-
+  //   console.log(`Server is running on ${process.env.BASE_URL}:${PORT}`);
+  // });
+  
 const express = require("express"); 
 const app = express(); 
 app.get("/", (req, res) => { res.send("Express on Vercel"); }); 
+
+const orderRoutes = require('./routes/orderRoutes');
+
+app.use('/api/orders', orderRoutes);
 
 const PORT = process.env.PORT || 5000;
 
