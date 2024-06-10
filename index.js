62d13444c2bf6d44fa596765b1223fee289f78ab
codescientists <piyushpp2412@gmail.com>
2024-06-10 12:43:12 +0530

    Fixed vercel error

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 8000;
-const cors = require('cors');
-app.use(cors());
+// const express = require('express');
+// const app = express();
+// const PORT = process.env.PORT || 8000;
+// const cors = require('cors');
+// app.use(cors());
 
-app.use(express.json());
+// app.use(express.json());
 
-const orderRoutes = require('./routes/orderRoutes');
+// const orderRoutes = require('./routes/orderRoutes');
 
-app.use('/api/orders', orderRoutes);
+// app.use('/api/orders', orderRoutes);
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on ${process.env.BASE_URL}:${PORT}`);
-});
+// // Start the server
+// app.listen(PORT, () => {
+//   console.log(`Server is running on ${process.env.BASE_URL}:${PORT}`);
+// });
+
+const express = require("express"); 
+const app = express(); 
+app.get("/", (req, res) => { res.send("Express on Vercel"); }); 
+
+const PORT = process.env.PORT || 5000;
+
+ app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
